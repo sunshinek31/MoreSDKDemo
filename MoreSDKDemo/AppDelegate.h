725488228937b2c981d1d51016e1d172730f5771
Jin Chen <sunshinek31@sunshinek31deMac-mini.local>
2015-01-19 10:59:35 +0800

    Initial Commit

---
+++ MoreSDKDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MoreSDKDemo
+//
+//  Created by sunshinek31 on 15/1/19.
+//  Copyright (c) 2015年 moneymoremore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
